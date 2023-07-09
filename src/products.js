6518c1e7c1df22396ffdacb51d472985b7160394
farhan <onihilinterit@gmail.com>
2023-07-09 12:38:17 +0700

    first commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "MacBook Air M2 (2022)",
+    price: 19999000.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 19999000.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "MacBook Pro M2 (2022)",
+    price: 19499000.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "MacBook Pro M1 16 Inci (2021)",
+    price: 32999000.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "MacBook Pro M1 14 Inci (2021)",
+    price: 26999000.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "MacBook Pro M1 (2020)",
+    price: 16499000.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "iMac 24 inci (M1, Empat Port, 2021)",
+    price: 25499000.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "iMac 24 inci (M1, Dua Port, 2021)",
+    price: 21999000.0,
+    productImage: product8,
+  },
+];
